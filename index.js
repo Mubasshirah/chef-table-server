1d6dfdc6c6e5cad615930eebffa021f517578b89
Mubasshirah <adibaju1994@gmail.com>
2023-05-04 17:08:26 +0600

    recipes data making

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express=require('express');
 const app=express();
 const port=process.env.PORT || 5000;
 const cors=require('cors');
+const chef=require('./data/chef.json')
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('chef table is running');
 });
+app.get('/chef',(req,res)=>{
+    res.send(chef);
+})
 app.listen(port,()=>{
     console.log(`chef table is running on port ${port}`)
 });
